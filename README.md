a38489ac6170d8e82cc4323be6d7efd6448b49a1
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2025-04-18 09:12:02 +0530

    chore: init application

---
+++ README.md
@@ -0,0 +1,3 @@
+# voting_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/voting_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.voting_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
